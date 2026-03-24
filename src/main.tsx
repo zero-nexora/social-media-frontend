403b73823e0b6eb4cb1361c3e6b559720672f675
DTH235651NguyenVanHien <hien_dth235651@student.agu.edu.vn>
2026-03-24 18:20:53 +0700

    fix router app

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,6 @@ import { createRoot } from "react-dom/client";
 import "quill/dist/quill.snow.css";
 import "./styles/quill-overrides.css";
 import "./index.css";
-import App from "./app";
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(<App />);
